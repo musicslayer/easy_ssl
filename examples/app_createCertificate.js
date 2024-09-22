cd54f8d16fadfd06355e131878eb1f46285e4880
musicslayer <musicslayer@gmail.com>
2024-09-22 14:18:22 -0400

    Update example

--- examples/app_createCertificate.js
+++ examples/app_createCertificate.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 const {createCertificate} = require("@musicslayer/easy_ssl");
+
+// This is NOT included in this package and must be installed separately i.e. npm install acme-dns-01-cloudflare
 const plugin_cloudflare = require("acme-dns-01-cloudflare");
 
 // Change these values based on your workflow.
-const IS_STAGING = true;
-const DOMAINS = ["a.yourdomain.com", "*.b.yourdomain.com"];
-const COULDFLARE_API_KEY = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+const IS_STAGING = true; // Set to true for testing. Set to false when you wish to create certificates for your real domains.
+const DOMAINS = ["a.yourdomain.com", "*.b.yourdomain.com"]; // Wildcards are permitted.
+const COULDFLARE_API_KEY = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // This API Key needs "Zone:Zone:Read" and "Zone:DNS:Edit" permissions.
 
 async function init() {
     let accountData = JSON.parse(fs.readFileSync("account_info.txt"));
